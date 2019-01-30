061a9553a4d4fca6b92106ac661ae1c332b5332a
boxdox <vaibhavkandwal@pm.me>
2019-01-30 23:02:40 +0530

    finished linting

--- main.py
+++ main.py
@@ -3,12 +3,13 @@
 """
 Author: Vaibhav Kandwal
 Website: www.vaibhavkandwal.com
-Description: This script allows you to Square Fit an image, by adding white boders to deficient side! For usage and info, refer to Github Repo
+Description: This script allows you to Square Fit an image, by adding white boders to 
+             deficient side! For usage and info, refer to Github Repo
 """
 
-from PIL import Image
 import ctypes
-import os, os.path
+import os
+from PIL import Image
 
 if os.name == "nt":
     clearkey = "cls"
@@ -16,7 +17,7 @@ if os.name == "nt":
 else:
     clearkey = "clear"
 
-color = (255, 255, 255)  # background color, change (this is in rgb format)
+color = (255, 255, 255)  # background color, change it to your liking. (rgb format)
 
 term_width = os.get_terminal_size().columns
 
@@ -25,34 +26,39 @@ print("-" * term_width)
 print("Image 1:1 Resizer".center(term_width))
 print("-" * term_width)
 print(
-    "\nI will resize all your images to square shape automatically in a particular directory (no need to enter any width or height). I can also add colored background if shape is rectangular (Yay!). Happy Resizing! :D \n\n"
+    "\nI will resize all your images to square shape automatically in a particular directory"
+    "(no need to enter any width or height). I can also add colored background if shape" 
+    "is rectangular (Yay!). Happy Resizing! :D \n\n"
 )
 
-
 def squarefit(filename):
-    old_im = Image.open(filename)  # open image file  
+    old_im = Image.open(filename)  # open image file
     sizew, sizeh = (
-        old_im.size 
+        old_im.size
     )  # since size method gives a tuple, split result into two variables
     newbig = max(sizew, sizeh)
-    new_im = Image.new("RGB", (newbig, newbig), color)  # create a new image with new size
-    new_im.paste(old_im, (int((newbig - sizew) / 2), int((newbig - sizeh) / 2))) # paste image opened in first line onto the newly created image
-    convertname = "converted/" + filename  # set filename with converted directory
-    new_im.save(convertname)  # save the image
+    new_im = Image.new(
+        "RGB", (newbig, newbig), color
+    )  # create a new image with new size
+    new_im.paste(
+        old_im, (int((newbig - sizew) / 2), int((newbig - sizeh) / 2))
+    )  # paste image opened in first line onto the newly created image
+    convert_name = os.path.join("converted/", filename)  # set filename with converted directory
+    new_im.save(convert_name)  # save the image
 
 
 filelist = (
     []
 )  # an array, that will contain list of all files to be converted. autopopulated below
 validext = [".jpg", ".gif", ".png", ".tga", ".jpeg"]  # valid extensions to search for
-loc = (
+location = (
     input("Enter Directory Containing Images [Enter for current Directory]: ")
     or os.getcwd()
 )
 
 
-os.chdir(loc)  # changedir
-for file in os.listdir(loc):  # find files which have valid extension
+os.chdir(location)  # changedir
+for file in os.listdir(location):  # find files which have valid extension
     ext = os.path.splitext(file)[1]
     if ext.lower() not in validext:
         continue
@@ -69,7 +75,8 @@ if len(filelist) != 0:
 else:
     os.system(clearkey)
     print(
-        "Ummm, looks like directory does not contain any images! Did you type the correct path?\n\nPlease run the script again!"
+        "Ummm, looks like directory does not contain any images! Did you type the correct path?"
+        "Please run the script again!"
     )
 input("\nPress Enter to continue ...")
 exit()
